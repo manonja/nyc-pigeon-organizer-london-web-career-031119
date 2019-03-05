625b18ff8698bed1cb23bfbee9a6b42a16c0bb99
manon jacquin <manon.jacquin@gmail.com>
2019-03-05 17:05:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+organized_list = {} 
+  data.each do |attributes, hash| 
+    hash.each do |specs, array|
+      array.each do |name|
+        if !organized_list.has_key?(name)
+        organized_list[name] = {} 
+        end 
+
+        if !organized_list[name].has_key?(attributes)
+        organized_list[name][attributes] = []
+        end
+
+        if !organized_list[name][attributes].include?(specs)
+        organized_list[name][attributes] << specs.to_s 
+        end
+      end
+    end
+  end
+  organized_list
 end
